557e49f6caaa475e957bbd5ab88b242dd4ac3768
frank-0908 <52615871+frank-0908@users.noreply.github.com>
2019-12-04 21:47:55 +0800

    -【】 使用多种方法读写文件 (#53)
    -【】 使用多种方法读写文件 (#53)

--- src/main/java/com/github/hcsp/io/FileAccessor.java
+++ src/main/java/com/github/hcsp/io/FileAccessor.java
@@ -1,23 +1,65 @@
 package com.github.hcsp.io;
 
-import java.io.File;
-import java.util.Arrays;
-import java.util.List;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.*;
 
 public class FileAccessor {
-    public static List<String> readFile1(File file) {}
+    public static List<String> readFile1(File file) throws IOException {
+        List<String> lines1 = IOUtils.readLines(new FileInputStream(file), "UTF-8");
+        return lines1;
+    }
 
-    public static List<String> readFile2(File file) {}
+    public static List<String> readFile2(File file) throws IOException {
+        List<String> lines2 = Files.readAllLines(file.toPath());
+        return lines2;
+    }
 
-    public static List<String> readFile3(File file) {}
+    public static List<String> readFile3(File file) throws IOException {
+        ArrayList<String> lines3 = new ArrayList<>();
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String string = null;
+        while ((string = br.readLine()) != null) {
+            lines3.add(string);
+        }
+        br.close();
+        fr.close();
+        return lines3;
+    }
 
-    public static void writeLinesToFile1(List<String> lines, File file) {}
+    public static void writeLinesToFile1(List<String> lines, File file) throws IOException {
+        FileUtils.writeLines(file, "UTF-8", lines);
+    }
 
-    public static void writeLinesToFile2(List<String> lines, File file) {}
+    public static void writeLinesToFile2(List<String> lines, File file) throws IOException {
+        Files.write(file.toPath(), lines);
+    }
 
-    public static void writeLinesToFile3(List<String> lines, File file) {}
 
-    public static void main(String[] args) {
+    public static void writeLinesToFile3(List<String> lines, File file) throws IOException {
+//        BufferedWriter asd=new BufferedWriter(new FileWriter(file.getName()));
+//        asd.write(lines.get(0));
+//        asd.write(lines.get(1));
+//        asd.write(lines.get(2));
+
+        FileWriter asd = new FileWriter(file);
+        BufferedWriter bw = new BufferedWriter(asd);
+        for (String str : lines) {
+            bw.write(str);
+            bw.newLine();
+        }
+        bw.flush();
+        asd.close();
+        bw.close();
+
+//        Files.newBufferedWriter(file.toPath(),lines,);
+    }
+
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         File testFile = new File(projectDir, "target/test.txt");
         List<String> lines = Arrays.asList("AAA", "BBB", "CCC");
