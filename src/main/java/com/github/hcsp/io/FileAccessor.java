6142227cc6ca209875198d499cf7fe28dd2c85be
zhy533726735220 <809775352@qq.com>
2019-12-12 20:07:46 +0800

    IO实战: 使用多种方法读写文件 (#55)
    IO实战: 使用多种方法读写文件 (#55)

--- src/main/java/com/github/hcsp/io/FileAccessor.java
+++ src/main/java/com/github/hcsp/io/FileAccessor.java
@@ -1,23 +1,92 @@
+
+////////////////////////////////////////////////////////////////////
+//                          _ooOoo_                               //
+//                         o8888888o                              //
+//                         88" . "88                              //
+//                         (| ^_^ |)                              //
+//                         O\  =  /O                              //
+//                      ____/`---'\____                           //
+//                    .'  \\|     |//  `.                         //
+//                   /  \\|||  :  |||//  \                        //
+//                  /  _||||| -:- |||||-  \                       //
+//                  |   | \\\  -  /// |   |                       //
+//                  | \_|  ''\---/''  |   |                       //
+//                  \  .-\__  `-`  ___/-. /                       //
+//                ___`. .'  /--.--\  `. . ___                     //
+//              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+//            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+//            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+//      ========`-.____`-.___\_____/___.-`____.-'========         //
+//                           `=---='                              //
+//      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+//            佛祖保佑       永不宕机      永无BUG                   //
+////////////////////////////////////////////////////////////////////
+
+
 package com.github.hcsp.io;
 
-import java.io.File;
+import org.apache.commons.io.FileUtils;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class FileAccessor {
-    public static List<String> readFile1(File file) {}
 
-    public static List<String> readFile2(File file) {}
+    public static List<String> readFile1(File file) throws IOException {
+        return FileUtils.readLines(file, Charset.defaultCharset());
+    }
 
-    public static List<String> readFile3(File file) {}
+    public static List<String> readFile2(File file) throws IOException {
 
-    public static void writeLinesToFile1(List<String> lines, File file) {}
+        List<String> results = new ArrayList<>();
 
-    public static void writeLinesToFile2(List<String> lines, File file) {}
+        FileReader fk = new FileReader(file.getAbsoluteFile());
 
-    public static void writeLinesToFile3(List<String> lines, File file) {}
+        BufferedReader br = new BufferedReader(fk);
 
-    public static void main(String[] args) {
+        String temp = null;
+
+        while ((temp = br.readLine()) != null) {
+
+            results.add(temp);
+        }
+
+        return results;
+
+    }
+
+    public static List<String> readFile3(File file) throws IOException {
+        return Files.readAllLines(Paths.get(file.getAbsolutePath()));
+
+    }
+
+    public static void writeLinesToFile1(List<String> lines, File file) throws IOException {
+        FileUtils.writeLines(file, lines);
+    }
+
+    public static void writeLinesToFile2(List<String> lines, File file) throws IOException {
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
+
+        for (String line : lines) {
+            bw.write(line);
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+
+    }
+
+    public static void writeLinesToFile3(List<String> lines, File file) throws IOException {
+        Files.write(Paths.get(file.getAbsolutePath()), lines);
+    }
+
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         File testFile = new File(projectDir, "target/test.txt");
         List<String> lines = Arrays.asList("AAA", "BBB", "CCC");
