9ea677ec14a1a84b11571b2adc87f894b7155b2d
Xi Pu <30915281+XiPu99@users.noreply.github.com>
2020-01-03 13:45:39 -0800

    read from and write to files using three different approaches (#61)
    read from and write to files using three different approaches (#61)

--- src/main/java/com/github/hcsp/io/FileAccessor.java
+++ src/main/java/com/github/hcsp/io/FileAccessor.java
@@ -1,21 +1,97 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class FileAccessor {
-    public static List<String> readFile1(File file) {}
+    public static List<String> readFile1(File file) {
+        try {
+            return Files.readAllLines(file.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Fail to read the file from " + file.getAbsolutePath());
+        }
+    }
 
-    public static List<String> readFile2(File file) {}
+    public static List<String> readFile2(File file) {
+        List<String> result = new ArrayList<>();
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            StringBuilder currLine = new StringBuilder();
+            while (true) {
+                int next = inputStream.read();
+                if (next == -1) {
+                    break;
+                } else if ((char) next == '\n') {
+                    result.add(currLine.toString());
+                    currLine.setLength(0);
+                } else {
+                    currLine.append((char) next);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Fail to read the file from " + file.getAbsolutePath());
+        }
+        return result;
+    }
 
-    public static List<String> readFile3(File file) {}
+    public static List<String> readFile3(File file) {
+        List<String> result = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            while (true) {
+                String line = bufferedReader.readLine();
+                if (line == null) {
+                    break;
+                } else {
+                    result.add(line);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Fail to read the file from " + file.getAbsolutePath());
+        }
+        return result;
+    }
 
-    public static void writeLinesToFile1(List<String> lines, File file) {}
+    public static void writeLinesToFile1(List<String> lines, File file) {
+        try {
+            Files.write(file.toPath(), lines);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Fail to write to file " + file.getAbsolutePath());
+        }
+    }
 
-    public static void writeLinesToFile2(List<String> lines, File file) {}
+    public static void writeLinesToFile2(List<String> lines, File file) {
+        try (FileOutputStream outputStream = new FileOutputStream(file)) {
+            for (String line : lines) {
+                byte[] currLine = new byte[line.length() + 1];
+                currLine[line.length()] = '\n';
+                for (int i = 0; i < line.length(); i++) {
+                    currLine[i] = (byte) line.charAt(i);
+                }
+                outputStream.write(currLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Fail to write to file " + file.getAbsolutePath());
+        }
+    }
 
-    public static void writeLinesToFile3(List<String> lines, File file) {}
+    public static void writeLinesToFile3(List<String> lines, File file) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            for (String line : lines) {
+                bufferedWriter.write(line);
+                bufferedWriter.write('\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Fail to write to file " + file.getAbsolutePath());
+        }
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
