bb794efcdccf68d0a90dedde0207375d28cedcc4
xdc <1434412784@qq.com>
2020-01-15 22:50:46 +0800

    草稿

--- src/main/java/com/github/hcsp/io/FileAccessor.java
+++ src/main/java/com/github/hcsp/io/FileAccessor.java
@@ -1,11 +1,17 @@
 package com.github.hcsp.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 
 public class FileAccessor {
-    public static List<String> readFile1(File file) {}
+    public static List<String> readFile1(File file) throws FileNotFoundException {
+        InputStream is = new FileInputStream(file);
+        is.
+    }
 
     public static List<String> readFile2(File file) {}
 
@@ -17,7 +23,7 @@ public class FileAccessor {
 
     public static void writeLinesToFile3(List<String> lines, File file) {}
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         File testFile = new File(projectDir, "target/test.txt");
         List<String> lines = Arrays.asList("AAA", "BBB", "CCC");
