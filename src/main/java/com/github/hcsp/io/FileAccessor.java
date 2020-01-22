a118aef3ff82c8ee04b6b3ea4aa36ccc6d3e860d
Yuzuki-N <59171699+Yuzuki-N@users.noreply.github.com>
2020-01-22 12:11:54 +0800

    使用多种方法读写文件 (#70)
    使用多种方法读写文件 (#70)

--- src/main/java/com/github/hcsp/io/FileAccessor.java
+++ src/main/java/com/github/hcsp/io/FileAccessor.java
@@ -1,23 +1,60 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import org.apache.commons.io.FileUtils;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class FileAccessor {
-    public static List<String> readFile1(File file) {}
+    public static List<String> readFile1(File file) throws IOException {
+        isExistence(file);
+        return FileUtils.readLines(file, "utf-8");
+    }
 
-    public static List<String> readFile2(File file) {}
+    public static List<String> readFile2(File file) throws IOException {
+        isExistence(file);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String contentLine = br.readLine();
+        List<String> list = new ArrayList<>();
+        while (contentLine != null) {
+            list.add(contentLine);
+            contentLine = br.readLine();
+        }
+        br.close();
+        return list;
+    }
 
-    public static List<String> readFile3(File file) {}
+    public static List<String> readFile3(File file) throws IOException {
+        isExistence(file);
+        Path path = Paths.get(file.getAbsolutePath());
+        return Files.readAllLines(path);
+    }
 
-    public static void writeLinesToFile1(List<String> lines, File file) {}
+    public static void writeLinesToFile1(List<String> lines, File file) throws IOException {
+        FileUtils.writeLines(file, "UTF-8", lines);
+    }
 
-    public static void writeLinesToFile2(List<String> lines, File file) {}
+    public static void writeLinesToFile2(List<String> lines, File file) throws IOException {
+        BufferedWriter bf = new BufferedWriter(new FileWriter(file));
+        for (String line : lines) {
+            bf.write(line + System.lineSeparator());
+        }
+        bf.flush();
+        bf.close();
+    }
 
-    public static void writeLinesToFile3(List<String> lines, File file) {}
+    public static void writeLinesToFile3(List<String> lines, File file) throws IOException {
+        Path path = Paths.get(file.getAbsolutePath());
+        Files.write(path, lines, StandardCharsets.UTF_8);
+    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         File testFile = new File(projectDir, "target/test.txt");
         List<String> lines = Arrays.asList("AAA", "BBB", "CCC");
@@ -28,5 +65,11 @@ public class FileAccessor {
         System.out.println(readFile1(testFile));
         System.out.println(readFile2(testFile));
         System.out.println(readFile3(testFile));
+    }
+
+    public static void isExistence(File file) {
+        if (!file.exists()) {
+            throw new RuntimeException("要读取的文件不存在");
+        }
     }
 }
