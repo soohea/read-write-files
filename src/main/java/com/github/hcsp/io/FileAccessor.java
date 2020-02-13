a6e429ff2b6d6081b8823e1dd6bfbfc6dd279eaf
YingHuaLiu <1042413535@qq.com>
2020-02-13 15:54:32 +0800

    使用多种方法读写文件

--- src/main/java/com/github/hcsp/io/FileAccessor.java
+++ src/main/java/com/github/hcsp/io/FileAccessor.java
@@ -1,23 +1,53 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class FileAccessor {
-    public static List<String> readFile1(File file) {}
+    //NIO
+    public static List<String> readFile1(File file) throws IOException {
+        return Files.readAllLines(file.toPath());
+    }
+    //Apache Commons IO
+    public static List<String> readFile2(File file) throws IOException {
+        return IOUtils.readLines(new FileInputStream(file), "UTF-8");
+    }
+    //BufferReader
+    public static List<String> readFile3(File file) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        List<String> list = new ArrayList<>();
+        String tmp;
+        while ((tmp = br.readLine()) != null) {
+            list.add(tmp);
+        }
+        br.close();
+        return list;
+    }
 
-    public static List<String> readFile2(File file) {}
+    //1. NIO
+    public static void writeLinesToFile1(List<String> lines, File file) throws IOException {
+        Files.write(file.toPath(), lines);
+    }
+    //2. Apache Commons IO
+    public static void writeLinesToFile2(List<String> lines, File file) throws IOException {
+        IOUtils.writeLines(lines, IOUtils.LINE_SEPARATOR, new FileOutputStream(file), "utf-8");
+    }
+    //3. BufferWriter
+    public static void writeLinesToFile3(List<String> lines, File file) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+        for (String tmp:lines){
+            bw.write(tmp+"\n");
+        }
+        bw.close();
+    }
 
-    public static List<String> readFile3(File file) {}
 
-    public static void writeLinesToFile1(List<String> lines, File file) {}
-
-    public static void writeLinesToFile2(List<String> lines, File file) {}
-
-    public static void writeLinesToFile3(List<String> lines, File file) {}
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         File testFile = new File(projectDir, "target/test.txt");
         List<String> lines = Arrays.asList("AAA", "BBB", "CCC");
