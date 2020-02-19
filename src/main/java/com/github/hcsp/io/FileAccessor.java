80f103f9cfbb884d64893f473e1b9f939673db12
Dygalo <59307187+dygalo@users.noreply.github.com>
2020-02-19 20:35:55 +0800

    read and write

--- src/main/java/com/github/hcsp/io/FileAccessor.java
+++ src/main/java/com/github/hcsp/io/FileAccessor.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static javafx.scene.input.DataFormat.FILES;
 
 public class FileAccessor {
     public static List<String> readFile1(File file) throws IOException {
@@ -17,15 +16,15 @@ public class FileAccessor {
     }
 
     public static List<String> readFile2(File file) throws IOException {
-        return  FileUtils.readLines(file,"UTF-8");
+        return FileUtils.readLines(file, "UTF-8");
 
     }
 
     public static List<String> readFile3(File file) throws IOException {
-        BufferedReader bufferedReader=new BufferedReader(new FileReader(file));
-        List<String> list=new ArrayList<>();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        List<String> list = new ArrayList<>();
         String line;
-        while ((line=bufferedReader.readLine()) !=null){
+        while ((line = bufferedReader.readLine()) != null) {
 
             list.add(line);
         }
