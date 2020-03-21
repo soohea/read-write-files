20c5556c82a78cd0b15cd22e727608309ef8c2c4
你çlinkai.huang <>
2020-03-21 23:54:18 +0800

    IO实战: 使用多种方法读写文件//删除第4种方法的单元测试

--- src/test/java/com/github/hcsp/io/FileAccessorTest.java
+++ src/test/java/com/github/hcsp/io/FileAccessorTest.java
@@ -3,7 +3,6 @@ package com.github.hcsp.io;
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -33,14 +32,6 @@ public class FileAccessorTest {
     }
 
     @Test
-    public void test4() throws Exception {
-        File tmp = File.createTempFile("tmp", "");
-        List<String> list = Arrays.asList("a", "b", " ", "   ", "c");
-        FileAccessor.writeLinesToFile4(list, tmp);
-        Assertions.assertEquals(list, FileAccessor.readFile4(tmp));
-    }
-
-    @Test
     public void testUnexisting() throws Exception {
         Assertions.assertThrows(
                 Exception.class, () -> FileAccessor.readFile1(new File("unexisting")));
@@ -48,7 +39,5 @@ public class FileAccessorTest {
                 Exception.class, () -> FileAccessor.readFile2(new File("unexisting")));
         Assertions.assertThrows(
                 Exception.class, () -> FileAccessor.readFile3(new File("unexisting")));
-        Assertions.assertThrows(
-                Exception.class, () -> FileAccessor.readFile4(new File("unexisting")));
     }
 }
