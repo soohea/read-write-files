b8f036310b738054ae594769f0d40f172babc921
pccmast <35651458+pccmast@users.noreply.github.com>
2020-11-01 20:26:18 +0800

    Pcc 2 (#146)
    Pcc 2 (#146)

--- src/main/java/com/github/hcsp/io/FileAccessor.java
+++ src/main/java/com/github/hcsp/io/FileAccessor.java
@@ -1,23 +1,67 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import org.apache.commons.io.FileUtils;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class FileAccessor {
-    public static List<String> readFile1(File file) {}
+    public static List<String> readFile1(File file) throws IOException {
+        // 使用 CommonIO 的 FileUtils 读取文件内容
+        return FileUtils.readLines(file);
+    }
 
-    public static List<String> readFile2(File file) {}
+    public static List<String> readFile2(File file) throws IOException {
+        // 使用NIO的 Files 工具类
+        return Files.readAllLines(file.toPath());
+    }
 
-    public static List<String> readFile3(File file) {}
+    public static List<String> readFile3(File file) throws IOException {
+        // 使用BufferedReader
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        List<String> results = new ArrayList<>();
+        String nextLine;
+        while ((nextLine = bufferedReader.readLine()) != null) {
+            results.add(nextLine);
+        }
+        bufferedReader.close();
+        fileReader.close();
+        return results;
+    }
 
-    public static void writeLinesToFile1(List<String> lines, File file) {}
+    public static void writeLinesToFile1(List<String> lines, File file) throws IOException {
+        // 使用 FilesUtils，FilesUtils不会自动换行，要手动添加
+        for (String line :
+                lines) {
+            FileUtils.write(file, line, true);
+            FileUtils.write(file, "\n", true);
+        }
+    }
 
-    public static void writeLinesToFile2(List<String> lines, File file) {}
+    public static void writeLinesToFile2(List<String> lines, File file) throws IOException {
+        // 使用 NIO 的 Files.write()
+        Files.write(file.toPath(), lines, StandardOpenOption.APPEND);
+    }
 
-    public static void writeLinesToFile3(List<String> lines, File file) {}
+    public static void writeLinesToFile3(List<String> lines, File file) throws IOException {
+        // 使用BufferedWriter输出
+        FileWriter fileWriter = new FileWriter(file.toString());
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        for (String line : lines) {
+            bufferedWriter.write(line);
+            bufferedWriter.newLine();
+        }
+        // 重要，没有close()会输出不了
+        bufferedWriter.close();
+        fileWriter.close();
+    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         File testFile = new File(projectDir, "target/test.txt");
         List<String> lines = Arrays.asList("AAA", "BBB", "CCC");
