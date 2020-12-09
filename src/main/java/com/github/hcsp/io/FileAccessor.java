9e905a3c1003c2997be2cb7384032c0cafa82d5a
wizard <42760426+qipengwei@users.noreply.github.com>
2020-12-10 00:48:03 +0800

    IO流 (#149)
    IO流 (#149)

--- src/main/java/com/github/hcsp/io/FileAccessor.java
+++ src/main/java/com/github/hcsp/io/FileAccessor.java
@@ -1,23 +1,59 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import org.apache.commons.io.FileUtils;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class FileAccessor {
-    public static List<String> readFile1(File file) {}
 
-    public static List<String> readFile2(File file) {}
+    public static List<String> readFile1(File file) throws IOException {
+        return FileUtils.readLines(file, Charset.defaultCharset());
+    }
 
-    public static List<String> readFile3(File file) {}
+    public static List<String> readFile2(File file) throws IOException {
+        //创建 FileReader
+        FileReader fileReader = new FileReader(file);
+        //转换为缓冲流
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        List<String> result = new ArrayList<>();
+        //循环读取
+        bufferedReader.lines().forEach(result::add);
+        //关闭流资源
+        bufferedReader.close();
+        return result;
+    }
 
-    public static void writeLinesToFile1(List<String> lines, File file) {}
+    public static List<String> readFile3(File file) throws IOException {
+        return Files.readAllLines(file.toPath());
+    }
 
-    public static void writeLinesToFile2(List<String> lines, File file) {}
 
-    public static void writeLinesToFile3(List<String> lines, File file) {}
+    public static void writeLinesToFile1(List<String> lines, File file) throws IOException {
+        FileUtils.writeLines(file, lines);
+    }
 
-    public static void main(String[] args) {
+    public static void writeLinesToFile2(List<String> lines, File file) throws IOException {
+        //转换流
+        FileWriter fileWriter = new FileWriter(file);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        //循环写入
+        for (String s : lines) {
+            bufferedWriter.write(s);
+            bufferedWriter.newLine();
+        }
+        bufferedWriter.close();
+    }
+
+    public static void writeLinesToFile3(List<String> lines, File file) throws IOException {
+        Files.write(file.toPath(), lines);
+    }
+
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         File testFile = new File(projectDir, "target/test.txt");
         List<String> lines = Arrays.asList("AAA", "BBB", "CCC");
@@ -28,5 +64,7 @@ public class FileAccessor {
         System.out.println(readFile1(testFile));
         System.out.println(readFile2(testFile));
         System.out.println(readFile3(testFile));
+
+
     }
 }
