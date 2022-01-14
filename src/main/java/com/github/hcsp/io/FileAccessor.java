5ffc0b2f11a371c2df6c05b2aaccdfd17b7c7dc1
UsernameisnotavailableFeng <91192169+UsernameisnotavailableFeng@users.noreply.github.com>
2022-01-14 15:51:30 +0800

    使用多种方法读写文件 (#178)
    使用多种方法读写文件 (#178)

--- src/main/java/com/github/hcsp/io/FileAccessor.java
+++ src/main/java/com/github/hcsp/io/FileAccessor.java
@@ -1,23 +1,54 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import org.apache.commons.io.FileUtils;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class FileAccessor {
-    public static List<String> readFile1(File file) {}
+    public static List<String> readFile1(File file) throws IOException {
+        return Files.readAllLines(file.toPath(), Charset.defaultCharset());
+    }
 
-    public static List<String> readFile2(File file) {}
+    public static List<String> readFile2(File file) throws IOException {
+        List<String> read = new ArrayList<>();
+        try (BufferedReader br = Files.newBufferedReader(file.toPath())) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                read.add(line);
+            }
+        }
+        return read;
+    }
+    public static List<String> readFile3(File file) throws IOException {
+        List<String> read = new ArrayList<>();
+        Stream<String> lines = Files.lines(file.toPath());
+        lines.forEach(read::add);
+        return read;
+    }
 
-    public static List<String> readFile3(File file) {}
+    public static void writeLinesToFile1(List<String> lines, File file) throws IOException {
+        Files.write(file.toPath(), lines);
+    }
+    public static void writeLinesToFile2(List<String> lines, File file) throws IOException {
+        FileUtils.writeLines(file, lines);
+    }
+    public static void writeLinesToFile3(List<String> lines, File file) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+        for (String line : lines) {
+            bufferedWriter.write(line);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+        }
+        bufferedWriter.close();
+    }
 
-    public static void writeLinesToFile1(List<String> lines, File file) {}
-
-    public static void writeLinesToFile2(List<String> lines, File file) {}
-
-    public static void writeLinesToFile3(List<String> lines, File file) {}
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         File testFile = new File(projectDir, "target/test.txt");
         List<String> lines = Arrays.asList("AAA", "BBB", "CCC");
@@ -28,5 +59,6 @@ public class FileAccessor {
         System.out.println(readFile1(testFile));
         System.out.println(readFile2(testFile));
         System.out.println(readFile3(testFile));
+
     }
 }
